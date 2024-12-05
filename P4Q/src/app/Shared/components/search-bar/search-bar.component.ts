53e2dec9ae1bc08b88bba981347f5268d0a1e7c8
Stefano <stefano.francolino@correo.ucu.edu.uy>
2024-12-04 21:06:32 -0300

    ..

---
+++ P4Q/src/app/Shared/components/search-bar/search-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-ticket',
+  imports: [],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class ModalTicketComponent {
+
+}
